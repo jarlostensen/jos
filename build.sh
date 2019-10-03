50472b0e6f8598f0404bfc7924136464f4d9c4a0
Jarl Ostensen <jarl.ostensen@gmail.com>
2019-10-03 08:09:02 +0100

    Fix: CRLF->LF....

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-# from meaty_skeleton build configuration
+
 SYSTEM_HEADER_PROJECTS="libc kernel"
 export PREFIX=/usr
 export EXEC_PREFIX=$PREFIX
 export BOOTDIR=/boot
 export LIBDIR=$EXEC_PREFIX/lib
 export INCLUDEDIR=$PREFIX/include
-# for cross compiling without any system libraries, expects this folder to contain stdio.h etc.
+
 export SYSROOT=$(pwd)/sysroot
-echo $SYSROOT
 mkdir -p "$SYSROOT"
-#for PROJECT in $SYSTEM_HEADER_PROJECTS; do
-#  (cd $PROJECT && DESTDIR="$SYSROOT" make install-headers)
-#done
+
+# copy system headers to sysroot as needed
+for PROJECT in $SYSTEM_HEADER_PROJECTS; do
+  (cd $PROJECT && DESTDIR="$SYSROOT" make install-headers)
+done
