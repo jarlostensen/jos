4e01ff8559c63df4e24918362b8af23e45790872
Jarl Ostensen <jarl.ostensen@gmail.com>
2019-10-04 06:40:49 +0100

    Fix: build script and makefiles for kernel + libc

--- build.sh
+++ build.sh
@@ -10,7 +10,10 @@ export INCLUDEDIR=$PREFIX/include
 export SYSROOT=$(pwd)/sysroot
 mkdir -p "$SYSROOT"
 
-# copy system headers to sysroot as needed
+# copy headers to sysroot as needed
 for PROJECT in $SYSTEM_HEADER_PROJECTS; do
   (cd $PROJECT && DESTDIR="$SYSROOT" make install-headers)
 done
+
+(cd libc && DESTDIR="$SYSROOT" make install)
+(cd kernel && DESTDIR="$SYSROOT" make iso)
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,5 +1,5 @@
 #include <stdint.h>
-#include "kernel.h"
+#include "../include/kernel/kernel.h"
 #include "dt.h"
 
 // =======================================================
