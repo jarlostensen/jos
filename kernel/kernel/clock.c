dba546461b976ddc0d1538b5fbec380f12d14bcb
jarlostensen <jarl.ostensen@gmail.com>
2019-10-16 16:59:04 +0100

    added atomic.h, test for one-shot PIT timer

--- kernel/kernel/clock.c
+++ kernel/kernel/clock.c
@@ -7,8 +7,6 @@
 
 #include <stdio.h>
 
-#define CLOCK_COMMAND 0x43
-#define CLOCK_DATA 0x40
 
 #define HZ              1000
 #define CLOCK_FREQ      1193182
@@ -17,15 +15,28 @@
 
 // http://www.brokenthorn.com/Resources/OSDev16.html
 
+#define PIT_COMMAND 0x43
+#define PIT_DATA_0  0x40
+#define PIT_DATA_2  0x42
+
 // PIT command word flags
-#define PIT_MODE_SQUAREWAVE     0x6     // binary, square wave
+#define PIT_COUNTER_0           0
+#define PIT_COUNTER_1           0x20
+#define PIT_COUNTER_2           0x40
+
+#define PIT_MODE_SQUAREWAVE     0x06     // binary, square wave
 #define PIT_RL_DATA             0x30    // LSB, then MSB
+#define PIT_MODE_ONESHOT        0x01
 
 uint32_t _k_clock_freq_frac = 0;
+//TODO: use atomic
 volatile uint32_t _k_clock_frac = 0;
 volatile uint64_t _k_ms_elapsed;
 // kernel.asm
 extern void _k_update_clock();
+
+// See
+// https://github.com/torvalds/linux/blob/master/arch/x86/kernel/tsc.c
 
 static void clock_irq_handler(int i)
 {
@@ -37,6 +48,19 @@ static void clock_irq_handler(int i)
 uint64_t k_get_ms_since_boot()
 {
     return _k_ms_elapsed;
+}
+
+void k_wait_oneshot_one_second()
+{
+    k_outb(PIT_COMMAND, PIT_COUNTER_2 | PIT_MODE_ONESHOT);
+    k_outb(PIT_DATA_2, 0xff);
+    k_outb(PIT_DATA_2, 0xff);
+
+    // dummy read, give time for the next edge rise
+    char msb = k_inb(PIT_DATA_2);
+    do {
+        msb = k_inb(PIT_DATA_2);
+    } while(msb);
 }
 
 void k_init_clock()
@@ -54,10 +78,10 @@ void k_init_clock()
     _k_update_clock();
     
     // initialise PIT
-    k_outb(CLOCK_COMMAND, PIT_MODE_SQUAREWAVE | PIT_RL_DATA);
+    k_outb(PIT_COMMAND, PIT_COUNTER_0 | PIT_MODE_SQUAREWAVE | PIT_RL_DATA);
     // set frequency         
-    k_outb(CLOCK_DATA, div16 & 0xff);
-    k_outb(CLOCK_DATA, (div16>>8) & 0xff);
+    k_outb(PIT_DATA_0, div16 & 0xff);
+    k_outb(PIT_DATA_0, (div16>>8) & 0xff);
 
     // start the clock IRQ
     k_set_irq_handler(0,clock_irq_handler);
